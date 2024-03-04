970520b06d9127ff47a029c44ac122e47b3ddf79
Isaac Katana <109860816+isaackatana@users.noreply.github.com>
2024-03-05 00:05:11 +0300

    +++

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import VitePluginSass from 'vite-plugin-sass'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePluginSass()],
-  base: "/isaac-katana",
+  base: "/isaackatana",
 })
