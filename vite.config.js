14935b945f9fe2f4daf1943b83db8cacd6d1fd11
Isaac Katana <109860816+isaackatana@users.noreply.github.com>
2024-03-04 23:24:29 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginSass from 'vite-plugin-sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginSass()],
   base: "/isaac-katana",
 })
