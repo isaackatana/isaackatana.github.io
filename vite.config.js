135da61aa107e5989185ed708f2018cfe325f5d4
Isaac Katana <109860816+isaackatana@users.noreply.github.com>
2024-03-05 01:38:53 +0300

    +++

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import VitePluginSass from 'vite-plugin-sass';
-import ViteGHPages from 'vite-plugin-gh-pages';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginSass(), ViteGHPages()],
+  plugins: [react(), VitePluginSass(),],
   base: "/isaackatana.github.io",
 })
