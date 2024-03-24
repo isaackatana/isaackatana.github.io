a3b50ffdb5269fd041eefeb6b5db1ca485af63b9
Isaac Katana <mrisaackatana@gmail.com>
2024-03-24 21:55:39 +0300

    +++

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,17 @@ function Home(props) {
       </div>
       <div className='container'>
         <div className="video">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/VyVVl_d2yZI?si=YQ1p6lM5G3qSSEzN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
+
         <div className='hero-info'>
-          <div className='join-course'>
-            <p>Join my online course to become a software developer</p>
-            <button>Join</button>
-            <FontAwesomeIcon icon={faArrowCircleRight} className='read-more'/>
-          </div>
+          
+        </div>
+
+        <div className='join-course'>
+          <p>Join my online course to become a software developer</p>
+          <button>Join</button>
+          <FontAwesomeIcon icon={faArrowCircleRight} className='read-more'/>
         </div>
       </div>
     </div>
