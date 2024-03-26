d095c9c4bc6ccd744a31f6eaa1498b279fd9fe8d
Isaac Katana <mrisaackatana@gmail.com>
2024-03-26 08:44:06 +0300

    +++

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Home(props) {
   return (
     <div className='hero'>
-      
+
       <div className="background-opacity">
 
       </div>
       <div className='hero-info'>
-        <h1>Hello I'm Isaac</h1>
+        <h1>Hello I'm <span>Isaac</span></h1>
         <p>Software Developer from Kenya</p>
       </div>
 
