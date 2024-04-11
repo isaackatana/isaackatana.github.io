51e20c549be266a9f147f592a55a208afb8a0a78
Isaac Katana <mrisaackatana@gmail.com>
2024-04-11 19:03:47 +0300

    +++

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,23 +14,25 @@ function Header() {
         <ul>
         </ul>
 
-        <nav>
-            <div className='social-media'>
-                <FontAwesomeIcon icon={faFacebook}/>
-                <FontAwesomeIcon icon={faInstagram}/>
-                <FontAwesomeIcon icon={faXTwitter}/>
-                <FontAwesomeIcon icon={faGithub}/>
-            </div>
+        <div className='menu'>
+            <nav>
+                <div className='social-media'>
+                    <FontAwesomeIcon icon={faFacebook}/>
+                    <FontAwesomeIcon icon={faInstagram}/>
+                    <FontAwesomeIcon icon={faXTwitter}/>
+                    <FontAwesomeIcon icon={faGithub}/>
+                </div>
 
-            <div className='user-acc'>
-                <button id='login'>Login</button>
-            </div>
-        </nav>
+                <div className='user-acc'>
+                    <button id='login'>Login</button>
+                </div>
+            </nav>
 
-        <div className='burger'>
-            <div className='line1'></div>
-            <div className='line2'></div>
-            <div className='line3'></div>
+            <div className='burger'>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
         </div>
     </header>
   )
