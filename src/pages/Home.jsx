79b2f2fd6038f8cd767e1648cc1e49bf9e9a16e2
isaackatana <mrisaackatana@gmail.com>
2024-04-19 00:23:36 +0300

    +++

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home({name}) {
       <div className='hero-info'>
         <h1>Hello I'm <span>{name}</span></h1>
         <p>I teach people how to make the web and how to make it look good while they're at it.</p>
-      </div>
+      </div>-
 
     </div>
   )
