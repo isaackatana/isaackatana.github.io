e4669de2bcf3b666f8ae14c802da95ef8e91df83
isaackatana <mrisaackatana@gmail.com>
2024-04-30 17:27:10 +0300

    +++

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,17 @@ function Home() {
   
   return (
     <div className='hero'>
-
-      <div className="background-opacity">
-
-      </div>
       
-      <div className='hero-info'>
-      </div>-
+      <div className="video-slide">
+
+        <div className="single-video">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/VyVVl_d2yZI?si=2WH3EzMeb8l9dh8E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div className="single-video">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/a5xMtNkjz-0?list=PLrEDJjV85iv4xU8PTIG1Bq9TA6rwP1HWM" title="Sonic Tambo -THE LOST FILES : Instrumental EP" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        
+      </div>
 
     </div>
   )
