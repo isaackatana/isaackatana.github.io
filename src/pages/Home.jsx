731def3decf026dd43771582adf9508aceee01cb
isaackatana <mrisaackatana@gmail.com>
2024-05-03 23:04:59 +0300

    +++

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ function Home() {
       <div className="video-slide">
 
         <div className="single-video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/VyVVl_d2yZI?si=2WH3EzMeb8l9dh8E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         
         
