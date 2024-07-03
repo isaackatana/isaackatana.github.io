742669cb9933513c5cb8400c73fd17e2ab12a1b4
isaackatana <your.mrisaackatana@gmail.com>
2024-07-03 19:27:36 +0300

    +++

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -22,7 +22,7 @@ function Portfolio() {
             <FontAwesomeIcon className='left' icon={faArrowAltCircleLeft}/>
             <FontAwesomeIcon className='right' icon={faArrowAltCircleRight}/>
             <div>
-              <iframe src={IntroVideo} frameborder="0"></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/LxcmF_Kk0Ho?si=anZUmV37oyN6ts4g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
           </div>
         </div>
