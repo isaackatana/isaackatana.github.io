8e8af0b791acaca742497478ab55bd66d4250fe1
isaackatana <your.mrisaackatana@gmail.com>
2024-07-17 20:22:09 +0300

    +++

--- src/pages/blog/PostContainer.jsx
+++ src/pages/blog/PostContainer.jsx
@@ -23,38 +23,40 @@ function PostContainer() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias. Exercitationem voluptate, natus dicta expedita ducimus eum iure, eos totam, perspiciatis ut dolor culpa error quo officiis animi dolorum nulla?</p>
         <img src="" alt="" />
         <iframe src="https://www.youtube.com/embed/LxcmF_Kk0Ho?list=PLrEDJjV85iv60M0AiFshvi7Xq3g3F48LJ" title="D-Berry - SO FINE  ft.Floh Namelok &amp; Gamerboy (lyric Visualizer)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <section className='user-opinion'>
-            <div className="react">
-                <FontAwesomeIcon icon={faThumbsUp}/>
-                <div>0</div>
-            </div>
-            <section className='share-on'>
-                <FontAwesomeIcon icon={faWhatsapp}/>
-                <FontAwesomeIcon icon={faFacebookF}/>
-                <FontAwesomeIcon icon={faXTwitter}/>
-            </section>
-        </section>
-        <section className='leave-a-comment'>
-            <div className="comment-input">
-                <form action="">
-                    <textarea name="" id="" placeholder='Leave a comment'></textarea>
-                    <div>
-                        <button>Post</button>
-                    </div>
-                </form>
-            </div>
-            <div className="comment-display">
-                <div className="comment">
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa sapiente illum modi nam harum consequuntur cupiditate labore, cum adipisci sequi!
-                    </p>
-                    <section className='react'>
-                        <FontAwesomeIcon icon={faThumbsUp}/>
-                        <div>0</div>
-                    </section>
+        <div className='wrapper'>
+            <section className='user-opinion'>
+                <div className="react">
+                    <FontAwesomeIcon icon={faThumbsUp}/>
+                    <div>0</div>
                 </div>
-            </div>
-        </section>
+                <section className='share-on'>
+                    <FontAwesomeIcon icon={faWhatsapp}/>
+                    <FontAwesomeIcon icon={faFacebookF}/>
+                    <FontAwesomeIcon icon={faXTwitter}/>
+                </section>
+            </section>
+            <section className='leave-a-comment'>
+                <div className="comment-input">
+                    <form action="">
+                        <textarea name="" id="" placeholder='Leave a comment'></textarea>
+                        <div>
+                            <button>Post</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="comment-display">
+                    <div className="comment">
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa sapiente illum modi nam harum consequuntur cupiditate labore, cum adipisci sequi!
+                        </p>
+                        <section className='react'>
+                            <FontAwesomeIcon icon={faThumbsUp}/>
+                            <div>0</div>
+                        </section>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
     <Blog/>
     </>
