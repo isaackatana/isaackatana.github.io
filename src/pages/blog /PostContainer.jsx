3e6566ca9aa7258e4fcc803ee63529c73deb94cc
isaackatana <your.mrisaackatana@gmail.com>
2024-07-25 22:08:35 +0300

    +++

--- src/pages/blog /PostContainer.jsx
+++ src/pages/blog /PostContainer.jsx
@@ -1,11 +1,11 @@
 'use client'
 import { faFacebookF, faWhatsapp, faXTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faArrowLeft, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useState} from 'react'
 import { Helmet } from 'react-helmet'
 import Blog from './Blog'
-import { Link, useParams } from 'react-router-dom'
+import {useParams } from 'react-router-dom'
 import { getBlogById } from '../../services/api'
 
 function PostContainer() {
@@ -36,15 +36,13 @@ function PostContainer() {
         <meta property="og:description" content="Software Developer" />
         <meta property="og:image" content="" />
     </Helmet>
-    <div className='previous-page'>
-        <Link to='/blog'><FontAwesomeIcon icon={faArrowLeft}/></Link>
-    </div>
     <div className="post-container">
         <h1>{blog.title}</h1>
         <p>{blog.content}</p>
         <img src="" alt="" />
         <iframe src="https://www.youtube.com/embed/LxcmF_Kk0Ho?list=PLrEDJjV85iv60M0AiFshvi7Xq3g3F48LJ" title="D-Berry - SO FINE  ft.Floh Namelok &amp; Gamerboy (lyric Visualizer)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <section className='user-opinion'>
+    </div>
+    <section className='user-opinion'>
             <section className='share-on'>
                 <h4>Share</h4>
                 <div>
@@ -53,38 +51,29 @@ function PostContainer() {
                 <FontAwesomeIcon icon={faXTwitter}/>
                 </div>
             </section>
-            <div className="react">
-                <span>0</span>
-                <FontAwesomeIcon icon={faThumbsUp}/>
+    </section>
+    <div className='wrapper'>
+        <section className='leave-a-comment'>
+            <div className="comment-input">
+                <form action="">
+                    <textarea name="" id="" placeholder='Leave a comment'></textarea>
+                    <div>
+                        <button>Post</button>
+                    </div>
+                </form>
             </div>
-        </section>
-        <div className='wrapper'>
-            <section className='leave-a-comment'>
-                <div className="comment-input">
-                    <form action="">
-                        <textarea name="" id="" placeholder='Leave a comment'></textarea>
-                        <div>
-                            <button>Post</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="comment-display">
-                    <div className="comment">
-                        <div className="profile-pic">
-                            <img src="" alt="" />
-                        </div>
-                        <div className='user-comment'>
-                            <h4>User</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, officia!</p>
-                            <section className='react'>
-                                <span>0</span>
-                                <FontAwesomeIcon icon={faThumbsUp}/>
-                            </section>
-                        </div>
+            <div className="comment-display">
+                <div className="comment">
+                    <div className="profile-pic">
+                        <img src="" alt="" />
+                    </div>
+                    <div className='user-comment'>
+                        <h4>User</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, officia!</p>
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
     <Blog/>
     </>
