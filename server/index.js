be00fd1e36ddf5f40a983621e6ffbf3951047e08
isaackatana <mrisaackatana@gmail.com>
2024-08-03 14:26:23 +0300

    +++

--- server/index.js
+++ server/index.js
@@ -9,15 +9,33 @@ app.use(express.json());
 const blogPosts = [
     {
         id: 1,
-        thumbnail: 'https://bigdataanalyticsnews.com/wp-content/uploads/2020/07/low-code-paltforms.png',
-        title: "Building Your First Web Application",
-        content: "An in-depth look at React Hooks and how they can simplify your code.",
+        thumbnail: 'https://www.byperth.com/wp-content/uploads/2018/04/stock-analyzer-project-architecture-1.jpg',
+        title: "The Benefits of Building an Application: Why It’s Worth the Investment",
+        content: "In today's digital world, building an application can be a game-changer for businesses, individuals, and organizations alike. Whether you're a startup founder, a seasoned entrepreneur, or simply passionate about technology, creating an app offers numerous advantages. Here’s a look at some of the key benefits:",
         video: 'https://www.youtube.com/embed/BfhSoFARn6w?si=JooHTOVjHXnl017-',
         dateCreated: new Date(),
-        slug: slugify('Building Your First Web Application', {lower:true})
+        slug: slugify('The Benefits of Building an Application: Why It’s Worth the Investment', {lower:true})
     },
     {
         id: 2,
+        thumbnail: 'https://bigdataanalyticsnews.com/wp-content/uploads/2020/07/low-code-paltforms.png',
+        title: "Getting Started with [Programming Language/Framework]",
+        content: "An in-depth look at React Hooks and how they can simplify your code.",
+        video: 'https://www.youtube.com/embed/BfhSoFARn6w?si=JooHTOVjHXnl017-',
+        dateCreated: new Date(),
+        slug: slugify('Getting Started with [Programming Language/Framework]', {lower:true})
+    },
+    {
+        id: 3,
+        thumbnail: 'https://imageio.forbes.com/specials-images/imageserve/636c1430f67770888c7fde53/Cash-dollars-and-stock-market-indicators/960x0.jpg?format=jpg&width=1440',
+        title: "Getting Started with [Programming Language/Framework]",
+        content: "An in-depth look at React Hooks and how they can simplify your code.",
+        video: 'https://www.youtube.com/embed/BfhSoFARn6w?si=JooHTOVjHXnl017-',
+        dateCreated: new Date(),
+        slug: slugify('Getting Started with [Programming Language/Framework]', {lower:true})
+    },
+    {
+        id: 4,
         thumbnail: 'https://imageio.forbes.com/specials-images/imageserve/636c1430f67770888c7fde53/Cash-dollars-and-stock-market-indicators/960x0.jpg?format=jpg&width=1440',
         title: "Getting Started with [Programming Language/Framework]",
         content: "An in-depth look at React Hooks and how they can simplify your code.",
