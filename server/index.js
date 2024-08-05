8740c3e6f46843b5503d6d8604856d2e351a4390
isaackatana <mrisaackatana@gmail.com>
2024-08-05 15:19:14 +0300

    +++

--- server/index.js
+++ server/index.js
@@ -6,6 +6,15 @@ const PORT = 3001;
 
 app.use(express.json());
 
+const path = require('path');
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const blogPosts = [
     {
         id: 1,
